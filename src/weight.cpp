0d7d9b48844ce45b4e58d5ea7b17aab9bf17f7b8
yang zhang <yzhang132@gmail.com>
2016-06-29 18:40:54 -0400

    c

--- src/weight.cpp
+++ src/weight.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <vector>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
@@ -18,28 +20,39 @@ int main()
 	vector<vector<double>> weight(180, vector<double>(540, 0));
 	
 	ifstream fdfp_mc(workdir + "/tables/dfpmc_si.dat", ios::in);
-	ifstream fweight("weight.dat", ios::in);
+	ofstream fweight("weight.dat", ios::out);
+    double norm = 0;
+
 	for(int i = 0; i < 180; ++i)
 	{
 		for(int j = 0; j < 540; ++j)
 		{
 			for(int k = 0; k < 5; ++k)
 			{
-				int ech, theta, proc, val;
+				int ech, theta, proc;
+                double val;
 				fdfp_mc >> ech >> theta >> proc >> val;
-				if(ech - 1 != i || theta - 1 != j || proc - 1 != k) return 1;
+				if(ech - 1 != i || theta - 1 != j || proc - 1 != k) 
+                {
+                    cout << "incorrect cross section table" << endl;
+                    return 1;
+                }
 				dfp_mc[i][j][k] = val;
-				if(k == 0) weight[i][j] += 7.7 * dfp_mc[i][j][0];
-				else if(k == 1) weight[i][j] += 1.0 * dfp_mc[i][j][0];
-				else if(k == 2) weight[i][j] += 1.0 * dfp_mc[i][j][0];
 			}
 			weight[i][j] = 7.7 * dfp_mc[i][j][0] + sqrt(7.7 * 1.0)
 							* (cos(0.8) * dfp_mc[i][j][1] + sin(0.8) * dfp_mc[i][j][4])
 							+ 1.0 * dfp_mc[i][j][2] + 0.5 * dfp_mc[i][j][3];
 			weight[i][j] *= eid[i];
-			fweight << setw(3) << i << setw(4) << j << setprecision(7) << fixed << setw(10) << weight << endl;
+            norm += weight[i][j];
 		}
 	}
+	for(int i = 0; i < 180; ++i)
+	{
+		for(int j = 0; j < 540; ++j)
+		{
+			fweight << setw(3) << i << setw(4) << j << setprecision(5) << scientific << setw(15) << weight[i][j]/norm << endl;
+        }
+    }
 	fweight.close();
 	return 0;
 }
